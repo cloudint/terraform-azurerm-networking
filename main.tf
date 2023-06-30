c0a418f9dcf655c1e9cf1d5e876e1f7dd6cb0a56
cloudint <65354068+cloudint@users.noreply.github.com>
2023-06-30 10:40:12 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "module" {
     version     = "v0.12.0"
   }
 }
-
+# 
 resource "azurerm_virtual_network" "module" {
   name                = "${local.module_name}-vnet"
   address_space       = var.vnet_address_spacing
